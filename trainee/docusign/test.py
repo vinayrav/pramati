5388b97b35f6acbb636df2520c9839c459df046c
vinayrav <vinayr0325@gmail.com>
2018-12-21 13:07:22 +0530

    docs

--- trainee/docusign/test.py
+++ trainee/docusign/test.py
@@ -1,33 +1,33 @@
 import requests
 
-account_id='31524907-e59d-46e6-b7f7-1ef93149f2a5'
-headers = {'Accept': 'application/json','Authorization': 'Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAAhra1tWTWSAgAAMbZw_hk1kgCAG3Tr024XZdBsUN30c1gEWIVAAEAAAAYAAEAAAAFAAAADQAkAAAANWFmMjA5MzQtYTNhNy00NWRlLWEwMDItZTg5NDE2YWFiY2U4MACAxa42l17WSA.5Hx4FRP_KNUXRMsCIvw3-8YDGBe3XPR532u_-rnVhTw8vCYT96VSt3m7zYYDiSwcNqX279RJhaSBva2nltX-85RkRyDZTMvtAgsKUWY8N-ra77bBNnO0ZI48SBa67CeqIYPWsOyiLsZhiIkpp467QejM7a5QWC5R4VcAYdwuqVq_QyXRgrTu7A8h4Be6ML9ydWRbnWBrrFCcz97iEcHri5IYCi8a4qro72EyuKuRGnmsqnyy8GE44Bb5hVVVw6sxg7baa_Dt1-jiQntflxr24ZzpF5BiwzZUeh0LxFiS3zwUdiWPhUJlREn4xUJFRixP-3ncrzC5f6j14p0wrTjxzA'}
+account_id='91f679ee-82b7-43b3-9603-71b39782269e'
+headers = {'Accept': 'application/json','Authorization': 'Bearer eyJ0eXAiOiJNVCIsImFsZyI6IlJTMjU2Iiwia2lkIjoiNjgxODVmZjEtNGU1MS00Y2U5LWFmMWMtNjg5ODEyMjAzMzE3In0.AQgAAAABAAUABwAAEQT_E2fWSAgAAFEnDVdn1kgCACRWOR8nUCtDiIV8E-dJYMIVAAEAAAAYAAEAAAAFAAAADQAkAAAANDk2NTJjOWUtOWEzMy00N2ViLWI2NzUtM2FmZjYyYjVlNGE0MACAd8qgNl_WSA.ywGJ_4fyyarLU4F13fBMKNocs0Rf-qLw1qaXI94KgAcKiG-IvMwK05nftbYGoqh-sKfZmTKDz1cOEtPo8ghHCIWV7Xnn30YZZrGxJnlgjwU2pQbJkS5lBPHgWnGLS1Id24u5JoxLHnKe_RFZY-IyL15sUA7QXbv30LROwtP-EdXoL_OvtoRQJhYK1mqshXnuz-64gBtgJYlIoa9afzDLz9Ug6zo_qEY5ecW2sPEQPPAJw4mfb-u5H6nbqes0zVyDVWGROa6_-XxvjroNTZa6osZASbA3Kz5YcPcTA1v6hKcI3us1AIJTNRhOQBnro6jDd8tJW60C8qjOsEq2OQQ2-A'}
 
 def recipientInfo():
-  url = 'https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/users?email=krohithvarma1997@gmail.com'
+  url = 'https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/users?email=vinayr0325@gmail.com'
   r = requests.get(url, headers=headers)
   print(r.status_code)
   print(r.text)
 
 def createEnvelope():
   url='https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/envelopes'
-  files = {'file': ('demo.json', open('demo.json', 'rb'), 'application/json')}
+  files = {'file': ('sign.json', open('sign.json', 'rb'), 'application/json')}
   r = requests.post(url, files=files, headers=headers)
   print(r.text)
 
 def envelopeDetails():
-  url='https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/envelopes/22c09a49-b2e6-48a8-8f5c-fdff642132b3/documents'
+  url='https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/envelopes/207e23e0-4e96-41e5-a9ea-c32d2f526b69/documents'
   r = requests.get(url,headers=headers)
   print(r.text)
 
 def downloadDocument():
-  url='https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/envelopes/22c09a49-b2e6-48a8-8f5c-fdff642132b3/documents/1'
+  url='https://demo.docusign.net/restapi/v2/accounts/'+account_id+'/envelopes/207e23e0-4e96-41e5-a9ea-c32d2f526b69/documents/1'
   r = requests.get(url,headers=headers)
-  with open('sampledoc.pdf', 'wb') as f:
+  with open('vny.pdf', 'wb') as f:
     f.write(r.content)
 
 
 recipientInfo()
-# envelopeDetails()
-# downloadDocument()
-# createEnvelope()
+envelopeDetails()
+downloadDocument()
+createEnvelope()
