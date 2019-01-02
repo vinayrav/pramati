ba805f2448448393afb14eff7dff962ea8b8773d
vinayrav <vinayr0325@gmail.com>
2019-01-02 10:49:34 +0530

    docjava

--- trainee/docujava/src/main/java/com/docusign/docujava/FileSend.java
+++ trainee/docujava/src/main/java/com/docusign/docujava/FileSend.java
@@ -1,17 +1,11 @@
 package com.docusign.docujava;
 
 import java.awt.Desktop;
-import java.awt.PageAttributes.MediaType;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.Base64;
 import java.util.Scanner;
-
-import javax.print.DocFlavor.URL;
-import javax.ws.rs.core.Request;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -38,14 +32,11 @@ String token_url="https://account-d.docusign.com/oauth/token";
 String callback_url="https://www.docusign.com";
 String secret_key="411f4c83-8848-41d4-80f4-86a6ccefe311";
     	 
-/*String[] auth_query_params= {"response_type"="code","scope"="signature","client_id"=client_id,"state"="a39fh23hnf23","redirect_uri"=callback_url};
-// construct the authorization url
-String final_auth_url = auth_url+"?{}".format(urllib.urlencode(auth_query_params));
-   */  
+ 
 String  response_type="code";
 String scope="signature";
 String state="a39fh23hnf23";
-//String url = "https://account-d.docusign.com/oauth/auth?response_type=,scope=,state" + URLEncoder.encode(response_type, "UTF-8");
+
 URIBuilder b = new URIBuilder(auth_url);
 b.addParameter("response_type", "code");
 b.addParameter("scope", "signature");
@@ -66,41 +57,35 @@ java.net.URL url = b.build().toURL();
     System.out.println("enter url");
    
      String authorization_response =input.nextLine();
-     System.out.println("authorization_response = " + authorization_response);
+    // System.out.println("authorization_response = " + authorization_response);
      
      String auth_code = authorization_response.split("\\=")[1];
-     //String stat = authorization_response.split("\\&")[1];
+     
      //getting authcode
      System.out.println(auth_code);
-    // System.out.println(stat);
-     
-     
-     //System.out.println("code = " + url.getcode());
-//String  response_type="code";
-     
-     
+       
     // BASE64_COMBINATION_OF_INTEGRATOR_AND_SECRET_KEYS
      String encodeBytes = Base64.getEncoder().encodeToString((client_id+":"+secret_key).getBytes());
   System.out.println("encoded value is " + encodeBytes);
   String  grant_type="authorization_code";
-  //String  code=" auth_code";
+  //Getting access token
   HttpResponse<JsonNode> response = Unirest.post("https://account-d.docusign.com/oauth/token")
 		  .header("Authorization", "Basic "+encodeBytes)
 		  .header("Content-Type", "application/x-www-form-urlencoded")
 		  .body("grant_type=authorization_code&code="+auth_code)
-		  .asJson();
-  
+		  .asJson(); 
     JSONObject tokenResponse=response.getBody().getObject();
-    System.out.println(tokenResponse);
+    //System.out.println(tokenResponse);
     String access_token=tokenResponse.getString("access_token");
-    System.out.println(access_token);
-    
+    //System.out.println(access_token);
+   
+    //Getting userinfo i.e baseuri and accountid
     HttpResponse<JsonNode> user_response = Unirest.get("https://account-d.docusign.com/oauth/userinfo")
   		  .header("Authorization", "Bearer "+access_token)
   		  .asJson();
     
       JSONObject user_info=user_response.getBody().getObject();
-      System.out.println(user_info);
+     // System.out.println(user_info);
 
      JSONArray accounts = user_info.getJSONArray("accounts");
       String base_uri=accounts.getJSONObject(0).getString("base_uri");
