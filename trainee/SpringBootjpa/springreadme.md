1641bf6f77c655d741908869dd1323ed7bcec253
vinayrav <vinayr0325@gmail.com>
2019-02-04 15:10:20 +0530

    spring

---
+++ trainee/SpringBootjpa/springreadme.md
@@ -0,0 +1,47 @@
+# Spring Boot Demo
+Minimal Spring Boot sample app.
+
+## Installation
+Download Spring Tool suite latest version
+ *https://spring.io/tools*
+
+## Usage
+```
+Create Spring Starter project
+Create inmemomry database h2
+                        i.e(spring.h2.console.enabled=true
+                            spring.datasource.platform=h2
+                            spring.datasource.url=jdbc:h2:mem:tab)
+Create an interface that extends JpaRepository.
+Create jsp (view page) in webapp folder i.e src->main-webapp->name.jsp
+(Mention the changes in application properties if the file is saved in other folder)
+```
+## Dependencies to include
+```
+<dependency>
+    <groupId>org.apache.tomcat</groupId>
+    <artifactId>tomcat-jasper</artifactId>
+    <version>9.0.14</version>
+</dependency>
+<dependency>
+    <groupId>com.h2database</groupId>
+    <artifactId>h2</artifactId>
+    <scope>runtime</scope>
+</dependency>
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-security</artifactId>
+    <version>2.1.2.RELEASE</version>
+</dependency>
+		
+
+```
+## Files Included
+**Files included in this project**
+- Data
+- DataController
+- Data Repo
+- Security Config
+- home.jsp
+- show.jsp
+- Data.sql
---
+++ trainee/SpringBootjpa/src/main/java/com/practice/demo/DataController.java
@@ -0,0 +1,81 @@
+package com.practice.demo;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class DataController {
+	
+	
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home.jsp";
+	}
+	
+	@Autowired
+	DataRepo datarepo;
+	
+	//@PostMapping is a composed annotation that acts as a shortcut for @RequestMapping(method = RequestMethod.POST)
+	@PostMapping(path="/data")    
+	public Data addData(@RequestBody Data daa) {
+		datarepo.save(daa);
+		return daa;
+	}
+	
+	
+	@GetMapping("/data") 
+	@ResponseBody
+	public List<Data> getData(){ 
+		return datarepo.findAll();
+	}
+	
+	
+	 @RequestMapping("/data/{bid}")
+	    @ResponseBody
+	    public Optional<Data> getidData(@PathVariable int bid){
+		return datarepo.findById(bid);
+	 }
+	
+	@DeleteMapping("/data/{bid}")
+	public String deleteData(@PathVariable int bid) {
+		Data d=datarepo.getOne(bid);
+		datarepo.delete(d);
+		return "deleted"+d;	
+	}
+	
+	@PutMapping("/data")
+	public Data CreateOrUpdate(@RequestBody Data data){
+		datarepo.save(data);
+		System.out.println("updated");
+		return data;
+	}
+	
+}
+
+
+	
+//	public ModelAndView getData(@RequestParam int bid) {
+//		ModelAndView mv=new ModelAndView("show.jsp");
+//		Data data=datarepo.findById(bid).orElse(null);
+//		mv.addObject(data);
+//		return mv;
+//	}
+
+	
+
+
+
+
